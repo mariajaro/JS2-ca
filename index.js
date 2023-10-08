17428d691a08120a43f8e10d9892d8b32ab53684
mariajaro <114582344+mariajaro@users.noreply.github.com>
2023-10-08 10:07:55 +0200

    made a js. for index

---
+++ index.js
@@ -0,0 +1,19 @@
+import { registerUser } from "./js/start/register.js";
+import { loginUser } from "./js/start/login.js";
+
+const login = document.getElementById("log-in");
+const register = document.getElementById("register");
+
+register.addEventListener("submit", () => {
+  event.preventDefault();
+  const [userName, email, password] = event.target.elements;
+  registerUser(userName, email, password);
+  register.reset();
+});
+
+login.addEventListener("submit", () => {
+  event.preventDefault();
+  const [email, password] = event.target.elements;
+  loginUser(email, password);
+  login.reset();
+});
