7aecd2f2327c118f4c646a1dc8e8c44099fbeaf7
mariajaro <114582344+mariajaro@users.noreply.github.com>
2023-10-08 10:44:52 +0200

    added register and log in

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-import { registerUser } from "./js/start/register.js";
-import { loginUser } from "./js/start/login.js";
+const baseURL = "https://api.noroff.dev/api/v1";
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const usernameInput = document.getElementById('username');
+const loginBtn = document.getElementById('loginBtn');
+const registerBtn = document.getElementById('registerBtn');
 
-const login = document.getElementById("log-in");
-const register = document.getElementById("register");
+// Registration Functionality
+registerBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
 
-register.addEventListener("submit", () => {
-  event.preventDefault();
-  const [userName, email, password] = event.target.elements;
-  registerUser(userName, email, password);
-  register.reset();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const name = usernameInput.value;
+
+    try {
+        const response = await fetch(baseURL + '/social/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+
+        if (response.ok) {
+            alert('Registration successful. Please log in.');
+        } else {
+            const data = await response.json();
+            alert('Registration error: ' + data.message);
+        }
+    } catch (error) {
+        console.error('Network error:', error);
+        alert('Network error. Please try again.');
+    }
 });
 
-login.addEventListener("submit", () => {
-  event.preventDefault();
-  const [email, password] = event.target.elements;
-  loginUser(email, password);
-  login.reset();
+// Login Functionality
+loginBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    try {
+        const response = await fetch(baseURL + '/social/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            // Save JWT to localStorage for future requests
+            localStorage.setItem('jwt', data.accessToken);
+            window.location.href = '/feed.html';  // redirect to the feed page
+        } else {
+            alert('Login error: ' + data.message);
+        }
+    } catch (error) {
+        console.error('Network error:', error);
+        alert('Network error. Please try again.');
+    }
 });
