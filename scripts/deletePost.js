38fec73a4ae67f8289a57c177afa81c80988fb9a
mariajaro <114582344+mariajaro@users.noreply.github.com>
2023-10-11 19:53:14 +0200

    made delete js

---
+++ scripts/deletePost.js
@@ -0,0 +1,22 @@
+export async function deletePost(postId) {
+  const API_URL = `https://api.noroff.dev/api/v1/social/posts/${postId}`;
+  const token = localStorage.getItem('accessToken'); // Ensure token is fetched 
+
+  try {
+      const response = await fetch(API_URL, {
+          method: 'DELETE',
+          headers: {
+              'Authorization': `Bearer ${token}`
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      alert("Post deleted successfully!");
+      fetchAndDisplayPosts();
+  } catch (error) {
+      console.error('Error deleting the post:', error);
+  }
+}
